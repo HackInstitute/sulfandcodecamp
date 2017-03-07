cf31f39b74893f6115c5302e5d34c8a61a5f2761
Jan Kus <jan@railslove.com>
2017-03-07 23:28:32 +0100

    added events to cta buttons

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,3 +1,15 @@
 ;(function(){
 
+	$('#cta-main-page-link').click(function(event) {
+	 	ga('send', 'event', 'registration', 'click', 'Call To Action Button Landingpage');
+	});
+
+	$('#menu-registration-link').click(function(event) {
+	 	ga('send', 'event', 'registration', 'click', 'Menu Registration Click');
+	});
+
+	$('#cta-subpage-link').click(function(event) {
+	 	ga('send', 'event', 'registration', 'click', 'Call To Action Button Subpage');
+	});
+
 })(jQuery)
